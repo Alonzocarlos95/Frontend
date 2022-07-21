0b801e52410c441814612463cc1b707626ac25e4
Carlos Alonzo <carlosalonzo95@hotmail.com>
2022-07-21 13:01:06 -0500

    add more functionalities

--- W14/exercises/js/index.js
+++ W14/exercises/js/index.js
@@ -1,5 +1,6 @@
 const bookCategories = ['Business','Fiction','Code','Art & Literature','Biography','Cooking','Drama','Education','Fantasy','Horror','Humor','Religious','Sports','Suspense','Thriller'];
 let myBookList = [];
+let bookCollection = {};
 
 function lookUpVolume(id){
     debugger;
@@ -11,6 +12,19 @@ function lookUpVolume(id){
             apiContent.description = data.volumeInfo.description
         })
         return apiContent;
+}
+
+function getRating(rate) {
+    const starTotal = 5;
+    let starTarget = document.querySelector(".stars-inner");
+    if(rate === undefined){
+        starTarget.style.width = "20%";
+    }
+    else {
+        const starPercentage = (rate / starTotal) * 100;
+        const starPercentageRounded = `${(Math.round(starPercentage / 10) * 10)}%`;
+        starTarget.style.width = starPercentageRounded;
+    }
 }
 
 function openModal(x) {
@@ -42,8 +56,20 @@ function openModal(x) {
         getBookId = x.target.parentNode.parentNode.lastChild.value;
     }
     // let getDataApi = lookUpVolume(getBookId);
+    for(let ref in bookCollection){
+        console.log(bookCollection[ref].Ikey);
+        if(getBookId === bookCollection[ref].Ikey){
+            document.getElementById("b_publisher").textContent = bookCollection[ref].info[0].authors[0];
+            document.getElementById("b_pages").textContent = bookCollection[ref].info[4].pageCount;
+            document.getElementById("book_title").textContent = bookCollection[ref].info[6].title;
+            document.getElementById("book_subtitle").textContent = bookCollection[ref].info[7].subtitle;
+            document.querySelector(".book-description").textContent = bookCollection[ref].info[8].description;
+            document.getElementById("published").textContent = getRating(bookCollection[ref].info[9].averageRating);
+            document.getElementById("published").textContent = bookCollection[ref].info[10].publishedDate;
+        }
+    }
     document.getElementById("book_avatar").src = getSrc;
-    document.querySelector(".book-description").textContent = getBookId;
+    
     domPropt.modal.classList.toggle("m-active");
 }
 
@@ -81,9 +107,10 @@ fetch("https://www.googleapis.com/books/v1/volumes?q=subject:javascript&printTyp
     .then(res => res.json())
     .then(data => {
         for(var i = 0; i < data.items.length;i++) {
-            domPropt.recommended.innerHTML += `<div class="re-books-wrapper"><div class="book-thumb"><img src="${domPropt.valThumbnail(data.items[i].volumeInfo)}" alt="${data.items[i].volumeInfo.title}"></div><span class="b-title">${data.items[i].volumeInfo.title}</span><p class="b-nAuthor"><strong class="b-nAuthor2">${domPropt.valAuthorName(data.items[i].volumeInfo)}</strong></p><input type="hidden" value="${data.items[i].volumeInfo.description}"></div>`;
+            bookCollection[i] = {"Ikey": data.items[i].id, "info": [{"authors": data.items[i].volumeInfo.authors},{"categories": data.items[i].volumeInfo.categories}, { "smallThumbnail": data.items[i].volumeInfo.imageLinks.smallThumbnail}, {"infoLink": data.items[i].volumeInfo.infoLink}, {"pageCount": data.items[i].volumeInfo.pageCount}, {"previewLink": data.items[i].volumeInfo.previewLink}, {"title":data.items[i].volumeInfo.title}, {"subtitle":data.items[i].volumeInfo.subtitle}, {"description":data.items[i].volumeInfo.description}, {"averageRating":data.items[i].volumeInfo.averageRating}, {"publishedDate":data.items[i].volumeInfo.publishedDate}]}
+            domPropt.recommended.innerHTML += `<div class="re-books-wrapper"><div class="book-thumb"><img src="${domPropt.valThumbnail(data.items[i].volumeInfo)}" alt="${data.items[i].volumeInfo.title}"></div><span class="b-title">${data.items[i].volumeInfo.title}</span><p class="b-nAuthor"><strong class="b-nAuthor2">${domPropt.valAuthorName(data.items[i].volumeInfo)}</strong></p><input type="hidden" value="${data.items[i].id}"></div>`;
         }
-
+        console.log(bookCollection);
     })
     .then(data => {
         console.log(domPropt.bookDetails);
